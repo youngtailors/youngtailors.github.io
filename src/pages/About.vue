ad842d5803b20599df77920ef984423adb746265
Hieu Tran Duc <hieutran.fu@gmail.com>
2019-01-07 14:18:37 +0700

    Initialize project

---
+++ src/pages/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <Layout>
+    <h1>About us</h1>
+    <p>We are young buffaloes and we are trying to become tailors</p>
+  </Layout>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Layout>
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    <h1>We want to say Hello World.</h1>
+    <p>We are developing this website to say more about our team.</p>
+  </Layout>
+</template>
+
+<script>
+export default {}
+</script>
